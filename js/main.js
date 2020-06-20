c7cf60ead4d65834c93d7cab1d57229c6b26dd75
Mike <maquinde@outlook.com>
2020-06-19 19:28:29 -0700

    still testing js. may need to start from scratch or rework the html?

--- js/main.js
+++ js/main.js
@@ -1,37 +1,72 @@
+
+
+
+
 const form = document.querySelector('#form');
 const button = document.querySelector('#submit');
 
 button.addEventListener('click', checkData);
 
-
-
-
 function checkData(event){
     event.preventDefault();
+    const fname = document.querySelector('#fname').value;
+    const lname = document.querySelector('#lname').value;
+    const email = document.querySelector('#email').value;
+    const pass = document.querySelector('#pass').value;
 
-    const fname = form['fname'].value;
-    const lname = form['lname'].value;
-    const email = form['email'].value;
-    const pass = form['pass'].value;
+    // const errorIcon = document.querySelector('.error-icon');
+    const errorSmall = document.querySelector('.error-small');
 
-    if(fname === ''){
-        //turn on error-icon and error-small. add error-border to form-control
-    } else {
-        //create function to remove classes or turn stuff off
-    }
     
+    if(fname === ''){
+        addError();
+    } else {
+        removeError();
+    }
+
+
+
+
 }
+
+
 
 
 function addError(){
-    
+    const fcontrol = form.fname.parentNode;
+    const img = document.createElement('img');
+    fcontrol.querySelector('.error').classList.add('error-border');
+    img.classList.add('error-icon');
+    img.src = './images/icon-error.svg';
+    fcontrol.querySelector('.img').appendChild(img);
+    fcontrol.querySelector('.error-small').style.display = 'block';
 }
+
+
+
 
 function removeError(){
+    const fcontrol = form.fname.parentNode;
+    const img = document.createElement('img');
 
+    fcontrol.querySelector('.error').classList.remove('error-border');
+    img.classList.remove('error-icon');
+    img.src = './images/icon-error.svg';
+    fcontrol.querySelector('.img').removeChild(img);
+    fcontrol.querySelector('.error-small').style.display = 'none';
 }
+
+
+
 
 function validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+// First name cannot be empty
+// Last name cannot be empty
+// Looks like this is not an email
+// Password cannot be empty
+
+// TODO: HOW TO ADD IMG SRC?
